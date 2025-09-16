65f5690b85128630862e13e9f420d8d3415870a5
Sketh <www.sketh007@gmail.com>
2025-09-16 21:08:47 +0530

    Basic Setup | Pause Logic
    - Setting resolution to 960x540
    - Making MainGameplayCanvas
        [=] Adding GridContainer
    - Adding BaseTile as separate scene prefab
    - Adding Pixel128x128 | Pause
    
    - Scripts
        [=] MainGameplayManager
            {+} Adding pause logic

--- Scripts/MainGameplayManager.gd
+++ Scripts/MainGameplayManager.gd
@@ -1,1 +1,36 @@
-extends Node
+extends Control
+
+# @export var _pauseTxt: Label
+
+# Buttons
+# @export var _pauseButton: Button
+
+#Textures
+@export var _pauseTex: CanvasItem
+@export var _playTex: CanvasItem
+
+var _pauseStatus: bool
+# var _debugStr: String
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+    _pauseStatus = false
+
+func _on_pause_button_pressed():
+    _pauseStatus = !_pauseStatus
+    get_tree().paused = _pauseStatus
+
+    if _pauseStatus:
+        self.process_mode = Node.PROCESS_MODE_WHEN_PAUSED
+        _pauseTex.hide()
+        _playTex.show()
+        # _pauseTxt.show()
+    else:
+        self.process_mode = Node.PROCESS_MODE_ALWAYS
+        _pauseTex.show()
+        _playTex.hide()
+        # _pauseTxt.hide()
+
+
+    print("_pauseStatus : " + str(_pauseStatus))
+    # self.process_mode = Node.PROCESS_MODE_DISABLED if _pauseStatus else Node.PROCESS_MODE_ALWAYS
