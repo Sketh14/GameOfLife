2d9f595c42deea41fe7d19c3fac22bc0dc95f1c3
Sketh <www.sketh007@gmail.com>
2025-09-18 00:53:48 +0530

    Initializing Tile 1
    - MainGameplayScene
        [=] Positioning Pause bt
        [=] Changing Container-Sizing for tiles to Expand | Also Minimum-Size
    - Scripts
        [=] MainGameplayManager
            {+} Adding _gridDimensions | _tileContainer | Starting Pos | Tile path
            {+} Initializing tiles logic

--- Scripts/MainGameplayManager.gd
+++ Scripts/MainGameplayManager.gd
@@ -9,14 +9,25 @@ extends Control
 @export var _pauseTex: CanvasItem
 @export var _playTex: CanvasItem
 
+# Tile Controls
+@export var _gridDimensions: Vector2i
+@export var _tileContainer: GridContainer
+# @export var _tilePrefab: Node
+
 var _pauseStatus: bool
 # var _debugStr: String
+
+#Constants
+const _TILE_STARTING_POS = Vector2(280, -16)
+const _TILE_PREFAB_PATH = "res://Scenes/BaseTile.tscn"
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
     _pauseStatus = false
+    _tileContainer.columns = _gridDimensions.y
+    InitializeTiles()
 
-func _on_pause_button_pressed():
+func OnPauseButtonPressed():
     _pauseStatus = !_pauseStatus
     get_tree().paused = _pauseStatus
 
@@ -34,3 +45,18 @@ func _on_pause_button_pressed():
 
     print("_pauseStatus : " + str(_pauseStatus))
     # self.process_mode = Node.PROCESS_MODE_DISABLED if _pauseStatus else Node.PROCESS_MODE_ALWAYS
+
+func InitializeTiles():
+    var tilePos = _TILE_STARTING_POS
+    var tilePrefab = load(_TILE_PREFAB_PATH)
+    var initializedTile
+    # print("_gridDimensions : " + str(_gridDimensions))
+    for i in _gridDimensions.x:
+        for j in _gridDimensions.y:
+            initializedTile = tilePrefab.instantiate()
+
+            # tilePos.x = 0.0
+            # tilePos.y = 0.0
+            initializedTile.position = tilePos
+
+            _tileContainer.add_child(initializedTile)
